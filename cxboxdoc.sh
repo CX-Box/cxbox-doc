b5b4645f3c59a9f8e26e240a503f5170c73d85fd
avgelfman <143416914+avgelfman@users.noreply.github.com>
2024-07-23 14:35:14 +0300

    Release v2.0.1 (#149)
    Release v2.0.1 (#149)

--- cxboxdoc.sh
+++ cxboxdoc.sh
@@ -7,7 +7,9 @@ echo 'Step3. download data'
 curl "http://code-samples.cxbox.org/api/v1/githubcode/" >/docs/out.zip
 echo 'Step4. unzip data'
 unzip /docs/out.zip
-echo 'Step5. files permission'
+echo 'Step5 delete out.zip'
+rm /docs/out.zip
+echo 'Step6. files permission'
 chmod -R 777 /docs/
-echo 'Step6. Start Application'
+echo 'Step7. Start Application'
 mkdocs serve --dev-addr=0.0.0.0:8000
